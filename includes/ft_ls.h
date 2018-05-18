b4373b406e9a57786c9ef96e61e177198823c9ee
inserttext <timothyngo08@gmail.com>
2018-05-17 18:21:22 -0700

    purge and restart

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: tingo <tingo@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/24 15:42:48 by tingo             #+#    #+#             */
-/*   Updated: 2018/05/15 20:38:51 by tingo            ###   ########.fr       */
+/*   Updated: 2018/05/17 18:04:49 by tingo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,5 @@
 # include <dirent.h>
 # include <sys/stat.h>
 # include <sys/types.h>
-
-# define ALL ((uint64_t)1<<('a'-'A'))
-# define LNG ((uint64_t)1<<('l'-'A'))
-# define TME ((uint64_t)1<<('t'-'A'))
-# define RCR ((uint64_t)1<<('R'-'A'))
-# define RVR ((uint64_t)1<<('r'-'A'))
-
-# define FLAG_MASK (ALL|LNG|TME|RCR|RVR)
-
-void	quick_sort(char **list, size_t low, size_t high);
-void	rquick_sort(char **list, size_t low, size_t high);
-void	invalid_option(char c);
-
-int		ls_core(uint64_t flags, char ***list);
-char	**listextend(char **list, char **item, size_t amt);
-size_t	countarg(char **arg);
 
 #endif
